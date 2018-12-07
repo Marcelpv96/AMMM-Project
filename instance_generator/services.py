9cce14e7dce3ef751b2e381a2e750e2108b30e54
Marcel Porta Vallés <marcelpv96@gmail.com>
2018-12-08 00:08:25 +0100

    Overlap time done

--- instance_generator/services.py
+++ instance_generator/services.py
@@ -33,7 +33,44 @@ class Services:
             overlaps += [[1 if services[s2].time < services[s1].end else 0 for s2 in range(0, len(services)-1)]]
         return overlaps
 
+    def get_starting_time(self):
+        return "[%s]" %  " ".join(list(map(str, [service.time for service in self.services])))
+
+    def get_duration_min(self):
+        return "[%s]" %  " ".join(list(map(str, [service.min for service in self.services])))
+
+    def get_duration_km(self):
+        return "[%s]" %  " ".join(list(map(str, [service.km for service in self.services])))
+
+    def get_overlap_time(self):
+        return "[%s]" %  " ".join(list(map(str, [service.time for service in self.services])))
+
+    def get_num_passangers(self):
+        return "[%s]" %  " ".join(list(map(str, [service.passangers for service in self.services])))
+
+    def get_overlap_time(self):
+        def process_lists(lists):
+            result = ""
+            for l in lists:
+                result += """
+                """
+                result += "[%s]" %  " ".join(list(map(str, l)))
+            return result
+        return """[
+        %s
+        ]""" %  process_lists(self.overlaps)
+
+    def to_string(self):
+        return """
+nServices=%d
+S_starting_time=%s
+duration_s_min=%s
+duration_s_km=%s
+overlap_time=%s
+num_passangers=%s""" % ((len(self.services), self.get_starting_time(), self.get_duration_min(), self.get_duration_km(),
+                        self.get_overlap_time(), self.get_num_passangers()))
+
 
 if __name__ == "__main__":
     s = Services(10, 10, [1,10], [1,10], [1,10], [1,10])
-    print(s.overlaps)
+    print(s.to_string())
